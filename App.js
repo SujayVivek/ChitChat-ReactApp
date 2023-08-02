dc05ad3ad7e6479e30d046a4aa5d6badbfadf20d
Sujay <sujayv2016@gmail.com>
2023-08-02 15:40:58 +0530

    done with 30% app

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import '../src/components/Dashboard_CSS.css';
+function App() {
+  return (
+    <>
+    <Navbar Title="ChitChat" />
+    <Dashboard />
+  </>
+  );
+}
+
+export default App;
